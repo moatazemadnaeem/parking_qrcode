5d0223fd74d059f3b946f88b61d5a3b7027a6bc0
moataz <moatazemad772@gmail.com>
2022-11-11 20:58:11 +0200

    fixed branch name function

--- utils/getBranchName.js
+++ utils/getBranchName.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 
 module.exports = function getCurrentBranchName(p = process.cwd()) {
-  const gitHeadPath = `${p}/.git/HEAD`;
-  if(fs.existsSync(p)&&fs.existsSync(gitHeadPath)){
+    const gitHeadPath = `${p}/.git/HEAD`;
+    // console.log('Branch Name-->',gitHeadPath,fs.readFileSync(gitHeadPath, 'utf-8'))
     return fs.readFileSync(gitHeadPath, 'utf-8').trim().split('/')[2];
-  }
-  return false;
 };
